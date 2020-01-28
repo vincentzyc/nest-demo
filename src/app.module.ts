992d85e57dc0126fa7aa90b3e72caf871366e417
Vincentyun <vincentzyc@qq.com>
2020-01-28 11:58:34 +0800

    UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module,HttpModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(),
     HttpModule,
     ServeStaticModule.forRoot({
       renderPath: join(__dirname, '..', 'public'),
       rootPath: join(__dirname, '..', 'public'),
     }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
