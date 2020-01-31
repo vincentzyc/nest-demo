865a255c0f9fdde20ee27e28462c94f8b5a71307
Vincentyun <vincentzyc@qq.com>
2020-01-31 20:41:54 +0800

    user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,20 +3,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 
+interface UserQuery {
+  pageSize?: number;
+  pageIndex?: number;
+}
+
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
-  ) {}
+  ) { }
 
-  async findAll(): Promise<UserEntity[]> {
-    const [roles, total] = await this.userRepository
+  async findAll(query: UserQuery): Promise<UserEntity[]> {
+    const [users, total] = await this.userRepository
       .createQueryBuilder('user')
-      .offset(1 - 1) // 从多少条开始
-      .limit(10) // 查询多少条数据
+      .offset((query.pageIndex || 1 - 1) * query.pageSize) // 从多少条开始
+      .limit(query.pageSize) // 查询多少条数据
       .getManyAndCount(); // 查询到数据及个数，返回的是一个数组
-      console.log(total);
-    return roles;
+    return users;
   }
 }
