3e6157aedd7dd837df8e9d8e77b10f8544978857
Vincentyun <vincentzyc@qq.com>
2020-02-02 13:27:29 +0800

    UserQuery

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get,Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserEntity } from './user.entity';
+import { UserResult } from './user.entity';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get()
-  findAll(@Query() query: Object): Promise<UserEntity[]> {
+  findAll(@Query() query: Object): Promise<UserResult> {
     return this.userService.findAll(query);
   }
 }
